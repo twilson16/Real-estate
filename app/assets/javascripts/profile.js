ff470cc56f25b6aa193a1993794599082d2ffd81
Tina <christinawilson6@gmail.com>
2019-02-07 02:35:12 -0500

    format and styling, no functionality

---
+++ app/assets/javascripts/profile.js
@@ -0,0 +1,18 @@
+var buttons = [
+  'Button 1',
+  'Button 2',
+  'Button 3',
+  'Button 4',
+  'Button 5',
+  'Button 6',
+  'Button 7',
+  'Button 8'
+]
+
+// buttons.forEach(button() {
+//     <div>button</div> <button />
+// } )
+
+buttons.each(function(index) {
+    <p>button</p> <button />
+}
